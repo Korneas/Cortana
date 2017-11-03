8c801d4d9f01a16c6dbe9001225718c38d4b6f88
korneas <cajomo0311@gmail.com>
2017-11-03 00:47:24 -0500

    Initial commit

---
+++ app/src/main/java/com/camilomontoya/cortana/CortanaControl.java
@@ -0,0 +1,146 @@
+package com.camilomontoya.cortana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.bluetooth.BluetoothSocket;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class CortanaControl extends AppCompatActivity {
+
+    private Button off;
+    private SeekBar intense;
+    private String address = null;
+    private ProgressDialog progress;
+    private BluetoothAdapter myBlue = null;
+    private BluetoothSocket bSocket = null;
+    private boolean isBConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    /**
+     *
+     * Conecta los elementos de el layout con valores que se editan en java
+     * para ser enviados mediante los Streams de Bluetooth
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fan_control);
+
+        Bundle newi = getIntent().getExtras();
+        address = newi.getString("EXTRA_ADDRESS");
+
+        off = (Button) findViewById(R.id.disconnect);
+
+        new ConnectBt().execute();
+
+        off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                turnOff();
+            }
+        });
+    }
+
+    /**
+     * Clase que hereda de AsyncTask para conocer si realmente con el apareado
+     * se tiene una conexion actual, si no existe la aplicacion se devuelve a la anterior
+     * pantalla para elegir otro dispositivo
+     */
+    private class ConnectBt extends AsyncTask<Void, Void, Void> {
+
+        private boolean connected = true;
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(CortanaControl.this, "Conectando...", "Por favor espere");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (bSocket == null || !isBConnected) {
+                    myBlue = BluetoothAdapter.getDefaultAdapter();
+                    BluetoothDevice dpst = myBlue.getRemoteDevice(address);
+                    bSocket = dpst.createInsecureRfcommSocketToServiceRecord(myUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    bSocket.connect();
+                }
+            } catch (IOException e) {
+                connected = false;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (!connected) {
+                aviso("Conexion fallida");
+                finish();
+            } else {
+                aviso("Conexion completa");
+                isBConnected = true;
+            }
+            progress.dismiss();
+        }
+    }
+
+    /**
+     *
+     * Metodo para enviar Strings a Arduino mediante el Bluetooth
+     *
+     * @param t String que se envia para ser leido por el Arduino
+     * @throws IOException
+     */
+    private void enviar(String t) throws IOException {
+        if(bSocket != null) {
+            bSocket.getOutputStream().write(t.toString().getBytes());
+            bSocket.getOutputStream().flush();
+        }
+    }
+
+    /**
+     * Metodo privado para enviar el mensaje de apagar el sistema de ventilacion
+     */
+    private void turnOff() {
+        try {
+            enviar("TF");
+        } catch (IOException e) {
+            aviso("Error de envio");
+        }
+
+        if (bSocket != null) {
+            try {
+                bSocket.close();
+            } catch (IOException e) {
+                aviso("Conexion apagada");
+            }
+            finish();
+        }
+
+        if (bSocket == null){
+            aviso("El dispositivo esta desconectado");
+        }
+    }
+
+    /**
+     * Metodo para acortar la generacion de Toast
+     *
+     * @param text String enviado para crear el mensaje
+     */
+    private void aviso(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/camilomontoya/cortana/MainActivity.java
@@ -0,0 +1,119 @@
+package com.camilomontoya.cortana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import java.util.Set;
+import java.util.ArrayList;
+import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
+/**
+ *
+ * MainActivity es el archivo java de la actividad principal donde se desarrolla la app,
+ * en la cual se requiere la lista de dispositivos bluetooth para conectar y un
+ * boton que refresque las conexiones
+ *
+ * @autor   Camilo Jose Montoya
+ * @version 0.1
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnPaired;
+    private ListView deviceList;
+
+    private BluetoothAdapter myBlue = null;
+    private Set pairedDevices;
+
+    /**
+     *
+     * Conecta al boton y la lista de dispositivos Bluetooth emparejados
+     * con el Java, para utilizar el click de el boton como busqueda de
+     * dispositivos emparejados y al seleccionar un elemento, ligar este
+     * como elemento para enviar informacion
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPaired = (Button) findViewById(R.id.button);
+        deviceList = (ListView) findViewById(R.id.listView);
+
+        myBlue = BluetoothAdapter.getDefaultAdapter();
+
+        if(myBlue == null){
+            aviso("Bluetooth no esta activado");
+            finish();
+        } else {
+            if(myBlue.isEnabled()){
+                Intent turnBtn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBtn,1);
+            }
+        }
+
+
+        btnPaired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pairedDevicesList();
+            }
+        });
+    }
+
+    /**
+     * Metodo para buscar los dispositivos emparejados y agregarlos a un ArrayList
+     * que luego se adapta al ListView
+     */
+    private void pairedDevicesList(){
+        pairedDevices = myBlue.getBondedDevices();
+        ArrayList list = new ArrayList();
+
+        if(pairedDevices.size()>0){
+            for (Object o: pairedDevices) {
+                BluetoothDevice bt = (BluetoothDevice) o;
+                list.add(bt.getName() + "\n" + bt.getAddress());
+            }
+        } else {
+            aviso("No hay dispositivos disponibles");
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        deviceList.setAdapter(adapter);
+        deviceList.setOnItemClickListener(myListClickListener);
+    }
+
+    /**
+     * Se crea un objeto AdapterView.OnItemClickListener para ser utilizado
+     * en el setOnItemClickListener de cada elemento del ListView
+     */
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length()-17);
+            Intent i = new Intent(MainActivity.this,CortanaControl.class);
+            i.putExtra("EXTRA_ADDRESS",address);
+            startActivity(i);
+        }
+    };
+
+    /**
+     * Metodo para acortar la generacion de Toast
+     *
+     * @param text String enviado para crear el mensaje
+     */
+    private void aviso(String text){
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+}
